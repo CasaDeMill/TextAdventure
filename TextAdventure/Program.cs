c08919dc548aa359b500fcab7cc64a8d9582c7d0
artinmall <artinmall96@gmail.com>
2019-05-15 16:15:51 +0200

    WIP

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -14,7 +14,7 @@ namespace TextAdventure
             PlayerStats newPlayer = new PlayerStats(Console.ReadLine(), 100, null);
             Console.Write("Welcome ");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(newPlayer.Name);
+            Console.WriteLine(newPlayer.Name + "!");
             Console.ResetColor();
             Console.WriteLine("Your health is: " + newPlayer.Health);
             Console.WriteLine("Your items are: " + newPlayer.Items);
