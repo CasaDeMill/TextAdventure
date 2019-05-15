921fe1456f98df5df8f2fc1743c6fee047738cac
artinmall <artinmall96@gmail.com>
2019-05-15 16:52:49 +0200

    WIP

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace TextAdventure
 {
@@ -10,8 +12,23 @@ namespace TextAdventure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
+            string textFileFolder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"TextFiles");
+
+            #region WelcomeInitialize
+
+            string[] welcome = File.ReadAllLines(textFileFolder + @"\Welcome.txt");
+            ConsoleColor c = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), welcome[0]);
+            Console.ForegroundColor = c;
+            foreach (string line in welcome.Skip(1))
+            {
+                Console.WriteLine(line);
+            }
+            Console.ResetColor();
             PlayerStats newPlayer = new PlayerStats(Console.ReadLine(), 100, null);
+
+
+            #endregion
+
             Console.Write("Welcome ");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(newPlayer.Name + "!");
